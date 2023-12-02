1f4984ebd76844f0ede2586f7697ad78daa98412
Eric Labaci <eric.labaci@gmail.com>
2023-12-02 07:35:55 -0300

    Creates project and adds Cartography and Alamofire dependencies

---
+++ F1 Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F1 Data
+//
+//  Created by Eric Labaci on 02/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
